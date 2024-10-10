5e65def3dccfdc896f83b504573e60ba0388353b
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-10-10 14:03:19 +0530

    Edited App Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./components/Header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={cn(inter.className, 'dark:bg-black dark:text-white')}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
