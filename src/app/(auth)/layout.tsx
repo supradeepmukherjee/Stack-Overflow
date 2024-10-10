41cb4c159dabd2e82de88043e3af403776303a1e
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-10-10 21:45:53 +0530

    Fixed Auth Component import error

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
+
 import { BackgroundBeams } from "@/components/ui/background-beams"
-import { cn } from "@/lib/utils"
 import { useAuthStore } from "@/store/auth"
 import { useRouter } from "next/navigation"
 import { ReactNode, useEffect } from "react"
@@ -21,19 +21,5 @@ const Layout = ({ children }: { children: ReactNode }) => {
         </div>
     )
 }
-
-
-export const BottomGradient = () => (
-    <>
-        <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from:transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
-        <span className="absolute inset-x-10 -bottom-px block mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
-    </>
-)
-
-export const LabelInputContainer = ({ children, className }: { children: ReactNode, className?: string }) => (
-    <div className={cn('flex w-full flex-col space-y-2', className)}>
-        {children}
-    </div>
-)
 
 export default Layout
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
 import { useAuthStore } from "@/store/auth"
 import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react"
 import Link from "next/link"
 import { FormEvent, useState } from "react"
-import { BottomGradient, LabelInputContainer } from "../layout"
 
 const Login = () => {
   const { login } = useAuthStore()
@@ -79,5 +79,24 @@ const Login = () => {
     </div >
   )
 }
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return <div className={cn("flex w-full flex-col space-y-2", className)}>{children}</div>;
+};
 
 export default Login
